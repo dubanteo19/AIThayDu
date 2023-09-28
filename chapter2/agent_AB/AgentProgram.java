589925e252bf5b6a0fc22acf184bb370ad360bf6
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2023-09-28 15:56:39 +0700

    first commit

---
+++ chapter2/agent_AB/AgentProgram.java
@@ -0,0 +1,17 @@
+package chapter2.agent_AB;
+
+import chapter2.agent_AB.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		Action re = null;
+		if(p.getLocationState()==LocationState.DIRTY)
+			re = Environment.SUCK_DIRT;
+		else if(p.getAgentLocation()==Environment.LOCATION_A)
+			re = Environment.MOVE_RIGHT;
+		else
+			re = Environment.MOVE_RIGHT;
+		return re;
+	}
+}
